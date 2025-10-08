00d9bab3d7f2bd35ae178c1fccd86cf90d9b7303
prit20032000 <84838659+prit20032000@users.noreply.github.com>
2025-10-08 16:49:47 +0530

    Update Time.cs

--- Log_Time_Finder_CSRA/ConsoleApp2/Time.cs
+++ Log_Time_Finder_CSRA/ConsoleApp2/Time.cs
@@ -2,8 +2,9 @@
 {
     public class Time
     {
-        public string? StartTime { get; set; }
+        public string? StartTimesd { get; set; }
         public string? EndTime { get; set; }
         public TimeSpan TimeDifference { get; set; }
     }
 }
+
